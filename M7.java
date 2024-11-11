47e7110c723f8762168bac04023528637a17c1cd
Vijeth737 <vijeth.23cs053@sode-edu.in>
2024-11-11 19:19:25 +0530

    Update and rename m7.java to M7.java

--- M7.java
+++ M7.java
@@ -1,26 +1,24 @@
-package pkg1;
-import pkg1.Tools2;
 class Tools2
 {
-    String s1="";
-    String s2="";
+    String s1=" ";
     int len1=0;
     public String reverse(String s1)
     {
+        String s2=" ";
         len1=s1.length();
         for(int i=0;i<len1;i++)
         {
             s2=s2+s1.substring(len1-(i+1),len1-i);
+            
         }
         return s2;
     }
 }
-
-public class m7
-{
-    public static void main(String [] args)
+public class M7 {
+    public static void main(String[] args)
     {
         Tools2 t1=new Tools2();
-        System.out.println(t1.reverse("Vijeth"));
+        System.out.println(t1.reverse("Vignesh"));
     }
+    
 }
